7d4651a2cc45cdcd3cdf8892f9a1fa1e32c82306
jtxukj <jtxukj@sunnyoptical.com>
2026-01-23 08:51:44 +0800

    ignore

---
+++ docs/src/.vitepress/theme/index.ts
@@ -0,0 +1,23 @@
+import DefaultTheme from 'vitepress/theme'
+import { ElementPlusContainer } from '@vitepress-demo-preview/component'
+import '@vitepress-demo-preview/component/dist/style.css'
+import 'element-plus/dist/index.css'
+import '@arco-design/web-vue/dist/arco.css'
+import ArcoVue from '@arco-design/web-vue'
+import { KunkkaIcon, KunkkaSearchInputTag, KunkkaBatchSelect, KunkkaSelect, KunkkaInput } from '@kunkka/ui'
+
+import './style.css'
+
+export default {
+  extends: DefaultTheme,
+  enhanceApp(ctx) {
+    const { app } = ctx;
+    app.component('demo-preview', ElementPlusContainer)
+    app.component('KunkkaIcon', KunkkaIcon)
+    app.component('KunkkaSearchInputTag', KunkkaSearchInputTag)
+    app.component('KunkkaBatchSelect', KunkkaBatchSelect)
+    app.component('KunkkaSelect', KunkkaSelect)
+    app.component('KunkkaInput', KunkkaInput)
+    app.use(ArcoVue)
+  }
+}
