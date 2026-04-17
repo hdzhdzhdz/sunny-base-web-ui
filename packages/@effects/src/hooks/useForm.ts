0f1194e4b0b52b08faa01554d3d879b29610cafb
SUNNYOPTICAL\fmchen <fmchen@sunnyoptical.com>
2026-04-17 16:46:23 +0800

    fix: 修改 useForm 默认配置中的 size 属性为 'small'

--- packages/@effects/src/hooks/useForm.ts
+++ packages/@effects/src/hooks/useForm.ts
@@ -16,7 +16,7 @@ interface UseFormOptions extends Partial<SunnyFormProps> {
 /** useForm 的默认配置，外部可 import 后进行覆盖/合并 */
 export const USE_FORM_DEFAULTS: Partial<SunnyFormProps> = {
   layout: 'vertical',
-  size: 'mini',
+  size: 'small',
   labelWidth: 'auto',
   gridProps: {
     xGap: 8,
